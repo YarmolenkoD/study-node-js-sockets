29b6aa44fc46fee9ce548435417f2335d5dc1a28
YarmolenkoDany <yarmolenko.dany@gmail.com>
2018-03-10 11:35:25 +0700

    change clientSecret field

--- app/configuration/config.js
+++ app/configuration/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     clientID: '157166725000122',
-    clientSecret: '04gM+EAfhnq4ALbhOX8jG5oRuow=',
+    clientSecret: '6ff6a2289b2ebe06ddc13e244c8a0701',
     callbackURL: 'https://polar-stream-90495.herokuapp.com/auth/facebook/callback',
     profileFields: ['id', 'name', 'displayName', 'picture', 'email']
 }
