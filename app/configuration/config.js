ff23253a91019a29909127de0eae1e19490f5010
Daniel Yarmolenko <dyarmolenko@Daniels-Mac-mini.local>
2018-03-10 06:08:40 +0200

    fix config

--- app/configuration/config.js
+++ app/configuration/config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  clientID: '157166725000122',
-  clientSecret: '04gM+EAfhnq4ALbhOX8jG5oRuow=',
-  callbackURL: 'https://polar-stream-90495.herokuapp.com/auth/facebook/callback',
-  profileFields: ['id', 'name', 'displayName', 'picture', 'email']
+  facebook: {
+    clientID: '157166725000122',
+    clientSecret: '04gM+EAfhnq4ALbhOX8jG5oRuow=',
+    callbackURL: 'https://polar-stream-90495.herokuapp.com/auth/facebook/callback',
+    profileFields: ['id', 'name', 'displayName', 'picture', 'email']
+  }
 }
